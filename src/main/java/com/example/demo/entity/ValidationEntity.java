3fad210611ec308abddf44b21978e2a9689b0128
bala-amypo <bala@amypo.in>
2025-12-18 08:08:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 
 @Entity
 public class ValidationEntity{
@@ -15,7 +17,10 @@ public class ValidationEntity{
     @NotNull
     @Size(min=2,max=10,message="must be 2 to 10 character")
     private String username;
+    @Email(message = "Email is not valid")
     private String email;
+    @Max(6)
+    @NotNull
     private String password;
     private int age;
 
