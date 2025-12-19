1256951cc4d904e1d12736a0ef446ed88ab52740
bala-amypo <bala@amypo.in>
2025-12-19 07:54:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/TimestampService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.entity.StudentEntity;
+public interface StudentService{
+    StudentEntity postData(StudentEntity stu);
+        //StudentEntity getData(); 
+    List<StudentEntity>getAllData();
+    String DeleteData(int id);
+    StudentEntity getData(int id);
+    StudentEntity updateData(int id,StudentEntity entity);
+
+}
