584fb077d5f311bb86221111495bfefc90417d91
bala-amypo <bala@amypo.in>
2025-12-19 10:47:15 +0000

    User commit

--- src/main/java/com/example/demo/service/IdDetailsService.java
+++ src/main/java/com/example/demo/service/IdDetailsService.java
@@ -1,1 +1,8 @@
-     
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.entity.IdDetails;
+public interface IdDetailsService{
+    Timestamp tpostData(Timestamp stu);
+        //StudentEntity getData(); 
+    
+}
