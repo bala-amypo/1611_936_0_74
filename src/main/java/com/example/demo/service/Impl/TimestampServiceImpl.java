8ae5d64ce0c992cb8ff1711093179ed9d826d906
bala-amypo <bala@amypo.in>
2025-12-19 08:00:20 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimestampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimestampServiceImpl.java
@@ -20,25 +20,5 @@ public class StudentServiceImpl implements StudentService{
         //StudentEntity getData();
         return student.save(stu);
     }
-    @Override
-    public List<StudentEntity>getAllData(){
-        return student.findAll();
-    }
-    @Override
-    public String DeleteData(int id){
-        student.deleteById(id);
-        return "Deleted Successfully";
-    }
-    @Override
-    public StudentEntity getData(int id){
-        return student.findById(id).orElse(null);
-    }
-    @Override
-    public StudentEntity updateData(int id,StudentEntity entity){
-        if(student.existsById(id)){
-            entity.setId(id);
-            return student.save(entity);
-        }
-        return null;
-    }
+    
 }
--- src/main/java/com/example/demo/service/TimestampService.java
+++ src/main/java/com/example/demo/service/TimestampService.java
@@ -1,12 +1,8 @@
 package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.Timestamp;
-public interface StudentService{
-    StudentEntity postData(StudentEntity stu);
+public interface TimestampService{
+    Timestamp postData(Timestamp stu);
         //StudentEntity getData(); 
-    List<StudentEntity>getAllData();
-    String DeleteData(int id);
-    StudentEntity getData(int id);
-    StudentEntity updateData(int id,StudentEntity entity);
-
+    
 }
