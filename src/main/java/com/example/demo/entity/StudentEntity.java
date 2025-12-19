62d2f0056b5b3fe1d05e59fc4c038c689b467938
bala-amypo <bala@amypo.in>
2025-12-19 06:27:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -14,46 +14,46 @@ public class StudentEntity{
     private String email;
     private String password;
     private Date created;
-    public Integer getid(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public Date getCreated(){
-        return created;
-    }
-    public void setId(Integer id){
-        this.id=id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setCreated(Date created){
-        this.created=created;
-    }
-    public StudentEntity(Integer id,String username,String email,String password,Date created){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.created=created;
-    }
-    public StudentEntity(){
+    // public Integer getid(){
+    //     return id;
+    // }
+    // public String getUsername(){
+    //     return username;
+    // }
+    // public String getEmail(){
+    //     return email;
+    // }
+    // public String getPassword(){
+    //     return password;
+    // }
+    // public Date getCreated(){
+    //     return created;
+    // }
+    // public void setId(Integer id){
+    //     this.id=id;
+    // }
+    // public void setUsername(String username){
+    //     this.username=username;
+    // }
+    // public void setEmail(String email){
+    //     this.email=email;
+    // }
+    // public void setPassword(String password){
+    //     this.password=password;
+    // }
+    // public void setCreated(Date created){
+    //     this.created=created;
+    // }
+    // public StudentEntity(Integer id,String username,String email,String password,Date created){
+    //     this.id=id;
+    //     this.username=username;
+    //     this.email=email;
+    //     this.password=password;
+    //     this.created=created;
+    // }
+    // public StudentEntity(){
 
-    }
+    // }
 
 
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -26,50 +26,50 @@ public class ValidationEntity{
     @Max(30)
     @Positive(message="Age must be a positive number")
     private int age;
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public int getAge(){
-        return age;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setAge(int age){
-        this.age=age;
-    }
-    public ValidationEntity(Long id,
-    @NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
-    @Email(message = "Email is not valid") String email,
-    @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message = "Password is mandatory")String password,
-    @Max(30) @Positive(message="Age must be a positive number") int age){
-        this.id=id;
-        this.username = username;
-        this.password=password;
-        this.email=email;
-        this.age=age;
-    }
-    public ValidationEntity(){
-        
-    }
-
     
-}
+//     public Long getId(){
+//         return id;
+//     }
+//     public String getUsername(){
+//         return username;
+//     }
+//     public String getEmail(){
+//         return email;
+//     }
+//     public String getPassword(){
+//         return password;
+//     }
+//     public int getAge(){
+//         return age;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public void setUsername(String username){
+//         this.username=username;
+//     }
+//     public void setEmail(String email){
+//         this.email=email;
+//     }
+//     public void setPassword(String password){
+//         this.password=password;
+//     }
+//     public void setAge(int age){
+//         this.age=age;
+//     }
+//     public ValidationEntity(Long id,
+//     @NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
+//     @Email(message = "Email is not valid") String email,
+//     @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message = "Password is mandatory")String password,
+//     @Max(30) @Positive(message="Age must be a positive number") int age){
+//         this.id=id;
+//         this.username = username;
+//         this.password=password;
+//         this.email=email;
+//         this.age=age;
+//     }
+//     public ValidationEntity(){
+        
+//     }
+
+ }
